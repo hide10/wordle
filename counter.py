5b6b371094cfd1c0019237a69c6c614cce45f49c
小林 秀人 <kobayashi@msint.com>
2022-02-02 15:45:03 +0900

    文字の出現数を数えるプログラムを追加

---
+++ counter.py
@@ -0,0 +1,11 @@
+counter = [0] * 26
+
+f = open('wordle.txt', 'r')
+
+lines = f.readlines()
+for line in lines:
+    for i in range(26):
+        counter[i] += line.count(chr(97+i))
+
+for i in range(26):
+    print(chr(97+i), counter[i])
