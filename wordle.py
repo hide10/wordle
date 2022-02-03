8ca4262ce97a2d750c511de762218b27d2481fd3
小林 秀人 <kobayashi@msint.com>
2022-02-03 09:25:37 +0900

    1回目でcorrect and wrong入力、2回目でnot in any spotを入力

--- wordle.py
+++ wordle.py
@@ -6,6 +6,8 @@ tmp_list = []
 f = open('wordle.txt', 'r')
 datalist = f.read().splitlines()
 
+print("correct and wrong:")
+
 in_str_list = list(input())
 
 for i, in_str in enumerate(in_str_list):
@@ -22,6 +24,15 @@ for i, in_str in enumerate(in_str_list):
         datalist = copy.copy(tmp_list)
         tmp_list.clear()
 
+print("not in any spot:")
+
+in_str_list = list(input())
+for i, in_str in enumerate(in_str_list):
+    for wd in datalist:
+        if not in_str.lower() in wd:
+            tmp_list.append(wd)
+    datalist = copy.copy(tmp_list)
+    tmp_list.clear()
 
 for str in datalist:
     print(str)
